94fb83e7c8d44c25fb058b8e5b687722930c17e5
manamu.fujiwara@gmail.com <ec2-user@ip-172-31-29-147.ec2.internal>
2021-03-11 15:01:12 +0000

    Commit1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  # 本の感想の投稿一覧画面および投稿画面のアクション
+  def index
+        # 投稿一覧画面のアクション
+    @books = Book.all
+    @book = Book.new
+  end
+
+  # 本の感想の投稿内容の保存
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id), notice:"Book was successfully created."
+  end
+
+  # 詳細ページ
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # 編集
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  # 更新
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id), notice:"Book was successfully updated."
+  end
+
+  # 削除
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to "/books"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
